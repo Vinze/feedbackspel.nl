40b25b48b72a9e9a8caf51603f4480e610a69358
Vincent Bremer <vbremer89@gmail.com>
2014-07-28 22:21:41 +0200

    New design, moved old files to backup zip

--- app/app.js
+++ app/app.js
@@ -3,39 +3,21 @@ var express          = require('express');
 var bodyParser       = require('body-parser');
 var cookieParser     = require('cookie-parser');
 var session          = require('express-session');
-var flash            = require('connect-flash')
-// var passport         = require('passport');
 var app              = express();
-
-// Load the config
-var config           = require('./config');
-
-// Load the controllers
-var HomeController   = require('./controllers/HomeController');
-var UserController   = require('./controllers/UserController');
 
 // Set the middleware
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
-app.use(cookieParser(config.secret));
-app.use(session({ secret: config.secret }));
-app.use(flash());
-
-// Set the auth stuff
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser('dsadsaasddsadsdfdfgfgdfgd'));
+app.use(session({ secret: 'dsadsaasddsadsdfdfgfgdfgd', saveUninitialized: true, resave: true }));
 
 // Routes
-app.get('/', HomeController.getIndex);
-app.get('/users', UserController.getIndex);
-app.get('/register', UserController.getRegister);
-app.post('/register', UserController.postRegister);
-app.get('/login', UserController.getLogin);
-app.post('/login', UserController.postLogin);
-
-app.get('/test', HomeController.getTest);
+app.get('/', function(req, res) {
+	res.render('home');
+});
 
 // Run the server
 app.listen(1337);
--- app/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	database: {
-		host     : 'localhost',
-		user     : 'root',
-		password : 'usbw',
-		database : 'feedbackspel.nl'
-	},
-	secret: 'fTTSl@LOWoWAP9%^U*AlT_PE^Mqhxk*A'
-}
