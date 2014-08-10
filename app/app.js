3a0ad20d0973543337692394b0efb6b0b01d5332
Vincent Bremer <vbremer89@gmail.com>
2014-08-10 22:55:33 +0200

    Added the NeDB database

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
 // Load the framework modules
-var express          = require('express');
-var bodyParser       = require('body-parser');
-var cookieParser     = require('cookie-parser');
-var session          = require('express-session');
-var app              = express();
+var express      = require('express');
+var bodyParser   = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+var nedb         = require('nedb');
+var app          = express();
 
 // Set the middleware
 app.set('views', __dirname + '/views')
