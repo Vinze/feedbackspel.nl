44925b14c61e86c1de45e5e2c8cc601ab639cdd5
Vincent Bremer <vbremer89@gmail.com>
2014-08-20 22:17:32 +0200

    Fixed sorting or array

--- app/public/js/dashboard.js
+++ app/public/js/dashboard.js
@@ -16,9 +16,31 @@ var resultList = new Ractive({
 			{ card: 'Zwijgzaam', comments: ['Je mag jezelf soms wel wat meer laten horen', 'Probeer meer naar de voorgrond te reden'], received: 5 }
 		],
 		sort: function (array) {
-			return _.sortBy(array, function(item) {
-				return item.received;
-			}).reverse();
+			// return array.sort( function ( a, b ) {
+			// 	return a['received'] > b['received'] ? -1 : 1;
+			// });
+
+			return array.sort(function(a, b) {
+				var received1 = a.received;
+				var received2 = b.received;
+
+				var card1 = a.card.toLowerCase();
+				var card2 = b.card.toLowerCase();
+				
+				if (received1 != received2) {
+					if (received1 > received2) return -1;
+					if (received1 < received2) return 1;
+					return 0;
+				}
+				if (card1 < card2) return -1;
+				if (card1 > card2) return 1;
+				return 0;
+
+			});
+			// return array;
+			// array = _.sortBy(array, function(item) {
+			// 	return item.received;
+			// }).reverse();
 		},
 		highest: function(array) {
 			var max = _.max(array, function(item) { return item.received });
@@ -30,7 +52,8 @@ var resultList = new Ractive({
 resultList.on({
 	open: function(evt) {
 		evt.original.preventDefault();
-		var index = evt.node.getAttribute( 'data-index' );
+		var index = evt.node.getAttribute('data-index');
+		console.log(index);
 		var selected = resultList.get('cards['+index+'].open');
 		resultList.set('cards['+index+'].open', !selected);
 	}
