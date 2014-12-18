9afa80ebcdac115176f16bd84fcd37404878151b
Vincent Bremer <vbremer89@gmail.com>
2014-12-18 14:43:06 +0100

    base URL in de config

--- app/libs/config.js
+++ app/libs/config.js
@@ -3,6 +3,7 @@ var os = require('os');
 var config = {
 	port: 1337,
 	token_expires: 365, // days
+	base: 'http://localhost:1337/',
 	jwt_secret: 'KwIeAC2Lu3toL1q7a88c',
 	cookie_secret: 'aBIaocaJ9EHpcDu4GsZy',
 	session_secret: '4rDl40UJSXq0lTUpPLWx',
@@ -11,6 +12,7 @@ var config = {
 
 if (os.hostname() == 'UbuntuVPS1') {
 	config.database = 'mysql://root:W3bW8woord@localhost/feedbackspel.nl';
+	config.base = 'http://feedbackspel.nl/';
 }
 
 module.exports = config;
